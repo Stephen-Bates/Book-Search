806fce7519502176a9f05475f674988d1f11bacc
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-05 17:31:05 -0400

    added blueprints for queries and mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+`;
+
+export const LOGIN = gql`
+`;
+
+export const SAVE_BOOK = gql`
+`;
+
+export const DELETE_BOOK = gql`
+`;
---
+++ client/src/utils/queries.js
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const GET_SINGLE_USER = gql`
+ 
+`;
+
+export const GET_ME = gql`
+ 
+`;

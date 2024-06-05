a913f6f4f2c5b0c602605fe48cddf8f3fa9726d1
Stephen Bates <Stephen.Michael.Bates@outlook.com>
2024-06-05 15:46:44 -0400

    added typeDefs for User and Book

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,19 @@
 const typeDefs = `
+  type User{
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+  }
 
+  type Book{
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
 `;
 
 module.exports = typeDefs;
